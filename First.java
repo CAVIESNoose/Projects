e461fe53f420d74824fae089c39e5f3932b3b3b8
Brayden Cavanaugh <brayden.cavie@gmail.com>
2026-08-22 10:20:06 -0400

    first commit

---
+++ First.java
@@ -0,0 +1,12 @@
+// Brayden Cavanaugh
+// First.java - Career Assignment
+// Prints greeting and future career info
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("Job title: Information Security Analyst");
+        System.out.println("Why: Strong job growth, aligns with my cybersecurity degree and current Elastic Engineering job");
+        System.out.println("Median salary: $124,910");
+    }
+}
